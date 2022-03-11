63a19f85882b607e183e8501212138497ebd5318
martyngodlewski1998 <martyngodlewski1998@gmail.com>
2022-03-11 18:19:19 +0100

    Added few serializers, enough progress to continue development of homepage

--- api/models.py
+++ api/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 
+def images_dir_path(instance, filename):
+    return f'imaged{filename}'
+
 class Product(models.Model):
     brand = models.CharField(max_length=20)
     model = models.CharField(max_length=30)
     price = models.DecimalField(max_digits=7, decimal_places=2)
-    #picture = models.ImageField(null=True, blank=True) // to add later 
+    picture = models.ImageField(upload_to=images_dir_path,null=True, blank=True)
     description = models.TextField(null=True, blank=True)
-
-    def __str__():
+    view_count = models.IntegerField(default=0)
+    amount = models.IntegerField(default=0)
+    def __str__(self):
         return f'{self.brand} {self.model}'
