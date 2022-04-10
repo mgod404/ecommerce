c1b2abd8885687d37fc479172565e52873b99629
Martyn <mgodlewski1998@gmail.com>
2022-04-10 13:41:59 +0200

    Changed href in navbar to useNavigate

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -1,17 +1,21 @@
 import {React} from "react"
+import { useNavigate } from "react-router-dom"
+
 import "bootstrap-icons/font/bootstrap-icons.css"
 import { Navbar, Container, Nav,} from "react-bootstrap"
-import './navbar.scss'
+import './navbar.scss';
 import NavbarCartComponent from "../navbarcart/navbarcart"
 
 const NavbarComponent = () => {
+    const navigate = useNavigate();
+
     return(
         <Navbar bg="dark" variant="dark" >
         <Container>
-            <Navbar.Brand href="/">Home</Navbar.Brand>
+            <Navbar.Brand onClick={() => navigate(`/`)}>Home</Navbar.Brand>
             <Nav className="me-auto">
-                <Nav.Link href="/smartphones/">Smartphones</Nav.Link>
-                <Nav.Link href="/laptops/">Laptops</Nav.Link>
+                <Nav.Link onClick={() => navigate(`/smartphones/`)}>Smartphones</Nav.Link>
+                <Nav.Link onClick={() => navigate(`/laptops/`)}>Laptops</Nav.Link>
             </Nav>
             <Nav className='justify-content-end'>
                 <NavbarCartComponent/>
