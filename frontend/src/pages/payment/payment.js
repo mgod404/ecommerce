7e7f86cd2e2fdec4e79e6a544efe134786299d78
Martyn <mgodlewski1998@gmail.com>
2022-04-13 18:44:41 +0200

    Finished new product functionality, started working on payments

---
+++ frontend/src/pages/payment/payment.js
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Card } from "react-bootstrap"
+import './payment.scss'
+
+const PaymentComponent = () =>{
+
+    return (
+    <div className='d-flex flex-row justify-content-center'>
+        <Card className='m-5'>
+            
+        </Card>  
+    </div>
+    )
+}
+
+export default PaymentComponent
