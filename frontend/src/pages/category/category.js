3ad1d9c5ec3defd93298c91d034e45c3e0329a7d
Martyn <mgodlewski1998@gmail.com>
2022-05-26 13:18:49 +0200

    Filter functionality almost done

--- frontend/src/pages/category/category.js
+++ frontend/src/pages/category/category.js
@@ -9,19 +9,31 @@ import { Form } from "react-bootstrap";
 const Category = (props) => {
     const [productData, setProductData] = useState(null);
     const [productFilters, setProductFilters] = useState({
-        'brand': [],
+        'brand__in': [],
         'price_max': '',
         'price_min': '',
         'ordering':''
     });
+    const [searchParams, setSearchParams] = useState([]);
+
+    const fetchProducts = async (searchParams = []) => {
+        let params = '';
+        Object.entries(searchParams).forEach( ([index, filter]) => {
+            console.log(filter);
+            params = params + `${filter.name}=${filter.value}&`;
+        });
+        let fetchURL = new URL(`http://127.0.0.1:8000/api/c/${props.category}/?${params}`);
+        console.log(`fn worked here is url ${fetchURL}`);
+        const response = await fetch(fetchURL);
+        const data = await response.json();
+        setProductData(data.result);
+        console.log(data);
+    }
 
 
     useEffect(() => {
-        fetch(`http://127.0.0.1:8000/api/search/?brand=${productFilters.brand}&category=${props.category}&ordering=${productFilters.ordering}&price_max=${productFilters.price_max}&price_min=${productFilters.price_min}`)
-        .then(res => res.json())
-        .then(data => setProductData(data.results));
-        console.log(productFilters);
-    }, [ props, productFilters ]);
+        fetchProducts();
+    }, []);
 
     return (
         <div>
@@ -29,6 +41,9 @@ const Category = (props) => {
                 category={props.category}
                 productFilters={productFilters}
                 setProductFilters={setProductFilters}
+                fetchProducts={fetchProducts}
+                searchParams={searchParams}
+                setSearchParams={setSearchParams}
                 />
                 <div className='d-flex flex-row justify-content-center'>
                     <div className="fs-5 text-center d-flex align-items-center">Sort by</div>
@@ -39,7 +54,7 @@ const Category = (props) => {
                         onChange={(e) => {
                             setProductFilters({
                                 ...productFilters,
-                                'ordering':e.target.value
+                                'order_by':e.target.value
                             });
                         }}
                         >
